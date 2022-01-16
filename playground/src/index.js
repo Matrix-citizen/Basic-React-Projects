43424520e59f1262889daf34a8df9845e475a4a8
Rostyslav Klyba <rklyba@yahoo.com>
2022-01-16 20:53:38 +0200

    Did basic setup of project 3

---
+++ playground/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+  },
+];
+
+const names = ['john', 'peter', 'susan']
+const newNames = names.map(name => <h1>{name}</h1>)
+function BookList() {
+  return (
+    <section className='booklist'>{newNames}</section>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Book = (props) => { 
+  const { img, title, author } = props;
+
+  return (
+    <article className='book'>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
