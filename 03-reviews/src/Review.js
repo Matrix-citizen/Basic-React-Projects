04d3561a0802e176d16f940e4058f7d7448a9ea7
Rostyslav Klyba <rklyba@yahoo.com>
2022-01-17 08:22:01 +0200

    Set functionality of 'Prev' and 'Next' button

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -5,6 +5,24 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) =>
+    number > people.length - 1 ? 0 : number < 0 ? people.length - 1 : number;
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -17,14 +35,14 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
-        <button className="random-btn">suprise me</button>
+      <button className="random-btn">suprise me</button>
     </article>
   );
 };
