15de873cb6021e4096080dc38cec541f28139931
Rostyslav Klyba <rklyba@yahoo.com>
2022-01-18 10:59:49 +0200

    Completed project 4

---
+++ playground/src/tutorial/1-useState/setup/4-useState-object.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+
+  // const [name, setName] = useState('peter');
+  // const [age, setAge] = useState(24);
+  // const [message, setMessage = useState('random message')];
+
+  const changeMessage = () => setPerson({ ...person, message: 'mymy' })
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
+};
+
+export default UseStateObject;
